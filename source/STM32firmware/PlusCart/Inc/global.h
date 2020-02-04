4b9d61a0fe5d572283756689ed2ae50cdf9ee7ef
Firmaplus(+) Ltd <w.stubig@firmaplus.de>
2020-02-04 14:19:42 +0100

    new version v.0.11.1

--- source/STM32firmware/PlusCart/Inc/global.h
+++ source/STM32firmware/PlusCart/Inc/global.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "stm32f4xx_hal.h"
 
-#define VERSION                   "0.11.0"
+#define VERSION                   "0.11.1"
 #define PLUSSTORE_API_HOST        "pluscart.firmaplus.de"
 
 #define TRUE 1
