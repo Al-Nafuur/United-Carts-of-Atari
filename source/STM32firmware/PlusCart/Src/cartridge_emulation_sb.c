93cee6824c09499700de3d6429362fedf2828155
Firmaplus(+) Ltd <w.stubig@firmaplus.de>
2020-07-08 14:04:09 +0200

    support for SB (SuperBanking)

---
+++ source/STM32firmware/PlusCart/Src/cartridge_emulation_sb.c
@@ -0,0 +1,42 @@
+
+#include <stdlib.h>
+#include "cartridge_setup.h"
+#include "cartridge_emulation_sb.h"
+#include "cartridge_firmware.h"
+
+void emulate_SB_cartridge( const char* filename, uint32_t image_size, uint8_t* buffer, MENU_ENTRY *d  )
+{
+	cartridge_layout * layout = (cartridge_layout *) malloc( sizeof(  cartridge_layout ));
+
+    if (!setup_cartridge_image(filename, image_size, buffer, layout, d, base_type_SB)) return;
+
+    uint8_t banks = ( image_size / 4096 ) - 1;
+    uint8_t *bank = layout->banks[banks];
+
+	uint16_t addr, addr_prev = 0;
+
+	if (!reboot_into_cartridge()) return;
+	__disable_irq();	// Disable interrupts
+
+	while (1)
+	{
+		while ((addr = ADDR_IN) != addr_prev)
+			addr_prev = addr;
+		// got a stable address
+		if (addr & 0x1000)
+		{ // A12 high
+			DATA_OUT = ((uint16_t)bank[addr & 0xFFF]);
+			SET_DATA_MODE_OUT
+			// wait for address bus to change
+			while (ADDR_IN == addr) ;
+			SET_DATA_MODE_IN
+		}else if (addr & 0x0800 ){ // addr > 0x7FF && addr < (0x800 + banks )){// (addr & 0xFF) < banks){	// bank-switch
+			bank = layout->banks[addr & banks];
+			// wait for address bus to change
+			while (ADDR_IN == addr) ;
+	    }
+	}
+	__enable_irq();
+
+	free(layout);
+}
