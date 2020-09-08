10fe16d26a0064dfd016e4038dd2c37cb65cefb1
Firmaplus(+) Ltd <w.stubig@firmaplus.de>
2020-09-08 17:32:20 +0200

    version bump to v0.14.15

--- source/STM32firmware/PlusCart/Inc/global.h
+++ source/STM32firmware/PlusCart/Inc/global.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "stm32f4xx_hal.h"
 
-#define VERSION                   "0.14.14"
+#define VERSION                   "0.14.15"
 #define PLUSSTORE_API_HOST        "pluscart.firmaplus.de"
 
 #define TRUE 1
