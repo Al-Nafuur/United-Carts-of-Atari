9c693687887af6f7d13d8cb53339ecb5b6b47338
Firmaplus(+) Ltd <w.stubig@firmaplus.de>
2021-02-06 02:23:22 +0100

    UnoCart GPIO fix

--- source/STM32firmware/PlusCart/Inc/cartridge_io.h
+++ source/STM32firmware/PlusCart/Inc/cartridge_io.h
@@ -9,13 +9,14 @@
 
 #if HARDWARE_TYPE == UNOCART
  #define DATA_IN ((uint16_t)(GPIOE->IDR))
- #define DATA_IN_BYTE (GPIOE->IDR & 0xFF)
+ #define DATA_IN_BYTE (GPIOE->IDR & 0xFF00)
  #define DATA_OUT GPIOE->ODR
  #define CONTROL_IN GPIOC->IDR
  #define SET_DATA_MODE_IN GPIOE->MODER = 0x00000000;
  #define SET_DATA_MODE_OUT GPIOE->MODER = 0x55550000;
  #define DATA_IN_SHIFT  >>8
- #define DATA_OUT_SHIFT  <<8
+ #define DATA_OUT_SHIFT <<8
+
 #else
  #define DATA_IN ((uint16_t)(GPIOC->IDR))
  #define DATA_IN_BYTE (GPIOC->IDR & 0xFF)
