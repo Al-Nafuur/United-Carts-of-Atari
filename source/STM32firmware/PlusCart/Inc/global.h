96507f5fe011854a8c4bf6bdd66ac3124833ab61
Firmaplus(+) Ltd <w.stubig@firmaplus.de>
2021-03-05 23:17:07 +0100

    Version bump

--- source/STM32firmware/PlusCart/Inc/global.h
+++ source/STM32firmware/PlusCart/Inc/global.h
@@ -20,7 +20,7 @@
 
 // End of hardware and periphery
 
-#define VERSION                   "2.0.13"
+#define VERSION                   "2.0.14"
 #define PLUSSTORE_API_HOST        "pluscart.firmaplus.de"
 
 #define CHARS_PER_LINE					32
