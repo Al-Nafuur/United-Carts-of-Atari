e7669f3b3ac7c6b6d28340610893dfd3d6b8a545
marcojohannes <mohhingman@yahoo.com>
2022-01-29 00:34:13 +1000

    Menu fix, comments, ACE interface version

--- source/STM32firmware/PlusCart/Src/cartridge_emulation_ACE.c
+++ source/STM32firmware/PlusCart/Src/cartridge_emulation_ACE.c
@@ -5,11 +5,16 @@
 #include "cartridge_emulation_ACE.h"
 #include "cartridge_firmware.h"
 
+#define ACE_MAJOR_REV 1
+#define ACE_MINOR_REV 0
+#define ACE_BUGFIX_REV 0
+
+
 // Adapted from the Unocart ACE scheme by ZackAttack.
 // ACE Implementation - For Pluscart. Adapted by Marco Johannes.
 // Uses the same header format as the Unocart scheme, except with a uinque magic number for Pluscart.
-// This header must exist at the beginning of every valid ace file
-// The bootloader automatically offsets the entry point if the user ROM is not at 0x0802000
+// This header must exist at the beginning of every valid ACE file
+// The bootloader automatically offsets the entry point if the user ROM is not at 0x0802000. For dynamic handling of user's offline rom storage.
 
 typedef struct __attribute__((packed)) {
 	uint8_t magic_number[8]; // Always ascii "ACE-PC00" for Pluscart ACE files
@@ -50,7 +55,7 @@ int launch_ace_cartridge( const char* filename, uint32_t image_size, uint8_t* bu
 	uint8_t* cart_rom = 0; //Set NULL pointer for now
 	void *EntryVector = 0; //Set NULL pointer for now
 
-	//Write to flash memory, or if offline run directly from flash.
+	//Write to flash memory, or if offline ROM then run directly from flash.
 	if(d->type == Cart_File ){
 
 #if USE_WIFI
@@ -72,23 +77,29 @@ int launch_ace_cartridge( const char* filename, uint32_t image_size, uint8_t* bu
     unsigned major = 0, minor = 0, bugfix = 0;
     sscanf(VERSION, "%u.%u.%u", &major, &minor, &bugfix); //Split up the Pluscart version number string into numeric form
 
-	//Setup pointers to be passed to ACE code in the beginning of buffer RAM. It is optional for the ACE application to use them though.
-	uint32_t* buffer32 = (uint32_t*)(0x20000000); //Create 32-bit pointer to buffer memory
-	*buffer32 = (uint32_t)cart_rom; //Declare the start position of the cart in flash. This changes depending on the state of the user's offline roms.
+	//Setup virtual arguments to be passed to ACE code in the beginning of buffer RAM. It is optional for the ACE application to use all of them though.
+	uint32_t* buffer32 = (uint32_t*)(0x20000000); //Set up temporary buffer pointer for writing arguments
+	*buffer32 = (uint32_t)cart_rom; //1. Declare the start position of the cart in flash as a 32 bit pointer. This changes depending on the state of the user's offline roms.
 	buffer32++;
-	*buffer32 = (uint32_t)(CCMPTR);//Declare the usage of CCM Memory so ACE application can use remainder, and not disturb the pluscart variables.
+	*buffer32 = (uint32_t)(CCMPTR);//2. Declare the usage of CCM Memory so ACE application can use remainder, and not disturb the Pluscart variables.
 	buffer32++;
-	*buffer32 = (uint32_t)(&reboot_into_cartridge); //Pass function pointer for reboot_into_cartridge
+	*buffer32 = (uint32_t)(&reboot_into_cartridge); //3. Pass Pluscart library function pointer for reboot_into_cartridge. Used for bootstrapping the 2600 system before running the ACE application.
 	buffer32++;
-	*buffer32 = (uint32_t)(&emulate_firmware_cartridge); //Pass function pointer for emulate_firmware_cartridge
+	*buffer32 = (uint32_t)(&emulate_firmware_cartridge); //4. Pass Pluscart library function pointer for emulate_firmware_cartridge. Used to exit out of a 2600 rom and back into the Pluscart menu.
 	buffer32++;
-	*buffer32 = (uint32_t)(SystemCoreClock);//Pass the system clock frequency for time dependent functions
+	*buffer32 = (uint32_t)(SystemCoreClock);//5. Pass the system clock frequency for time dependent functions.
 	buffer32++;
-	*buffer32 = (uint32_t)((major<<16)+(minor<<8)+bugfix);//Pass the Pluscart Firmware version number to the ACE application,encoded as  "00",major,minior,bugfix as a UINT32.
+	*buffer32 = (uint32_t)((ACE_MAJOR_REV<<16)+(ACE_MINOR_REV<<8)+ACE_BUGFIX_REV);//6. Pass the ACE Interface version number to the ACE application,encoded as  "00",major,minior,bugfix as a UINT32. The ACE code can then accept or reject compatibility before execution.
+	buffer32++;
+	*buffer32 = (uint32_t)((major<<16)+(minor<<8)+bugfix);//7. Pass the Pluscart Firmware version number to the ACE application,encoded as  "00",major,minior,bugfix as a UINT32. The ACE code can then accept or reject compatibility before execution.
+	buffer32++;
+	*buffer32 = (uint32_t)(0xACE42600);//8. Argument list termination magic number
+
+//Stock code uses "EntryVector" below, this jumps to the function pointer set in the ACE header(and adjusted by the bootloader dependent on the state of offline roms).
+//The ROM based ACE code has no arguments. However, "virtual" arguments are passed in the form of a uint32_t lookup table in buffer memory (0x20000000). It is optional for the ROM ace code to use all of the arguments.
+//This arrangement exists so that the ARM stack is unchanged for future versions of ACE which might pass more virtual arguments. In c, functions need to have the exact same number and type of arguments in their library code as from the function that called them.
 	
-//Stock code uses "EntryVector" below. Use emulate_ACEROM_cartridge for potentially quicker test cycle for ACE code in test firmware.
-	((void (*)())EntryVector)(); /*Uncomment this line to run in ACE mode (code in ROM)*/
+	return ((int (*)())EntryVector)(); /*Uncomment this line to run in ACE mode (code in ROM)*/
 //	emulate_ACEROM_cartridge();  /*Uncomment this line to run in firmware mode(code in Pluscart fimrware) */
 
-	return 1;
 }
--- source/STM32firmware/PlusCart/Src/main.c
+++ source/STM32firmware/PlusCart/Src/main.c
@@ -51,6 +51,7 @@
 #include "flash.h"
 #include "cartridge_io.h"
 #include "cartridge_firmware.h"
+#include "cartridge_emulation_ACE.h"
 #include "cartridge_emulation_ar.h"
 #include "cartridge_detection.h"
 #include "cartridge_emulation.h"
@@ -58,7 +59,7 @@
 #include "cartridge_emulation_bf.h"
 #include "cartridge_emulation_sb.h"
 #include "cartridge_emulation_dpcp.h"
-#include "cartridge_emulation_ACE.h"
+
 
 void generate_udid_string(void);
 
@@ -1261,8 +1262,8 @@ void emulate_cartridge(CART_TYPE cart_type, MENU_ENTRY *d)
 	else if (cart_type.base_type == base_type_ACE)
 	{
 		static unsigned char CCMUsageFinder __attribute__((section(".ccmram#"))); //Method of finding where allocation has reached for CCM RAM
-		uint32_t* CCMpointer=&CCMUsageFinder;
-		launch_ace_cartridge(curPath, cart_size_bytes, buffer, d, offset, cart_type.withPlusFunctions,CCMpointer);
+		uint32_t* CCMpointer=(uint32_t*)&CCMUsageFinder; //Find address of CCM allocation and cast as a pointer
+		launch_ace_cartridge(curPath, cart_size_bytes, buffer, d, offset, cart_type.withPlusFunctions,CCMpointer); //Open the ACE bootloader library function
 	}
 
 #if USE_WIFI
@@ -1450,18 +1451,17 @@ int main(void)
 						if(cart_type.uses_systick){
 							SysTick_Config(SystemCoreClock / 1000U);	// 1KHz
 						}
-						if (cart_type.uses_ccmram) {
-							truncate_curPath(); // ?? twice ?
-							d->type = Sub_Menu;
-							buildMenuFromPath(d);
-						}
 					}
 
 					else
 						menuStatusMessage = romtype_unknown;
 				}
 
-				truncate_curPath(); // ?? twice ?
+				truncate_curPath();
+
+					d->type = Sub_Menu;
+					buildMenuFromPath(d);
+
 
 			}
 
